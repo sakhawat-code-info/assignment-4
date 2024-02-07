377e0a94a4ecf9afb05c9294fefb8f931d78c54d
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-07 22:05:19 +0600

    problem 5 done onle 4 unclear

--- a.js
+++ a.js
@@ -13,41 +13,85 @@ function calculateMoney(ticketSellCount) {
 // console.log(print);
 
 
-function checkName(str) {
+// function checkName(str) {
 
-    if (typeof str === 'string') {
-        const lastChar =str[str.length-1];
-        const lastCharLowerCase = lastChar.toLowerCase();
+//     if (typeof str === 'string') {
+//         const lastChar =str[str.length-1];
+//         const lastCharLowerCase = lastChar.toLowerCase();
     
-        if (lastCharLowerCase === "a" || lastCharLowerCase === "y" || lastCharLowerCase === "i" || lastCharLowerCase === "e" || lastCharLowerCase === "o" || lastCharLowerCase === "u" || lastCharLowerCase === "w") {
-            return "Good Name";
-        }else{
-            return "Bad Name";
-        }
-    }else{
-        return "invalid";
-    }
+//         if (lastCharLowerCase === "a" || lastCharLowerCase === "y" || lastCharLowerCase === "i" || lastCharLowerCase === "e" || lastCharLowerCase === "o" || lastCharLowerCase === "u" || lastCharLowerCase === "w") {
+//             return "Good Name";
+//         }else{
+//             return "Bad Name";
+//         }
 
+//     }else{
+//         return "invalid";
+//     }
+
+// }
+
+// const print = checkName(['Rashed']);
+// console.log(print);
+
+
+// function deleteInvalids(array) {
+//     if(Array.isArray(array)){
+        
+//         const returnArray = [];
+//         for (const items of array) {
+//             if (typeof items === "number" && Number.isNaN(items) === false) {
+//                 returnArray.push(items);
+//             }
+//         }
+//         return returnArray;
+//     }else{
+//         return "Invalid Array";
+//     }
+// }
+
+// const print = deleteInvalids({num: [ 1 , 2 , 3 ]});
+// console.log(print);
+
+
+
+function password(obj) {
+    
 }
 
-const print = checkName(['Rashed']);
+const print = password({  birthYear: 1999 , siteName: "google" });
 console.log(print);
 
 
 
 
 
+// function monthlySavings(earningArray, livingCost) {
+//     if (Array.isArray(earningArray) && typeof livingCost === 'number') {
+        
+//         let taxPay = 0;
+//         let totalIncome = 0;
+//         for (const items of earningArray) {
+//             if (items > 3000) {
+//                 taxPay = (items * 20) / 100;
+//             }
+//             totalIncome += items;
+//         }
 
+//         let saving = totalIncome - (taxPay + livingCost);
 
+//         if (saving > 0) {
+//             return saving;
+//         }else {
+//             return "earn more";
+//         }
 
-
-
-// function deleteInvalids() {
-    
+//     }else{
+//         return "Invalid input";
+//     }
 // }
-// function password() {
-    
-// }
-// function monthlySavings() {
-    
-// }
+
+
+
+// const print = monthlySavings(100, [ 900 , 2700 , 3400]);
+// console.log(print);
