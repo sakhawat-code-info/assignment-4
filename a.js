562c1d5c7e6fdd852961b282cddc2c5e514f4b31
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-02-08 15:34:44 +0600

    Question added

--- a.js
+++ a.js
@@ -15,12 +15,28 @@
 // const print = deleteInvalids({num: [ 1 , 2 , 3 ]});
 // console.log(print);
 
+function password(obj) {
+    if (obj.hasOwnProperty("name") === true && obj.hasOwnProperty("birthYear") && obj.hasOwnProperty("siteName") && obj.birthYear > 1000 && obj.birthYear < 10000) {
+    
+        const objSiteName = obj.siteName;
+        const firstObjNameChar = objSiteName.charAt(0);
+        const firstObjNameCharCap = firstObjNameChar.toUpperCase();
+        const wordWithoutFirstChar = objSiteName.slice(1);
+
+        const finalCapSideName = firstObjNameCharCap + wordWithoutFirstChar;
+    
+        const output = finalCapSideName + "#" + obj.name + "@" + obj.birthYear;
+
+        return output;
+    }else{
+        return "invalid";
+    }
+}
 
 
 
-
-// const print = password({ name: "maisha" , birthYear: 2002 });
-// console.log(print);
+const print = password({ name: "maisha" , birthYear: 2002 });
+console.log( print);
 
 
 
